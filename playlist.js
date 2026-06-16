0a2cb40ab6bb8507568ef8dd2c8d660b771efbf4
Denis SOARES <opx7a862u@mozmail.com>
2026-06-16 23:28:37 +0200

    fix AL by default

--- playlist.js
+++ playlist.js
@@ -1,5 +1,4 @@
-// Généré par config.html — 16/06/2026 22:54:52
-window.PLAYLIST_FR = [
+window.PLAYLIST_FR=[
   "01-001 Gabriel.mp3",
   "1-01 L'aventurier.mp3",
   "1-02 Tu m'oublieras.mp3",
@@ -34,8 +33,7 @@ window.PLAYLIST_FR = [
   "2-15 Monday, Tuesday... Laissez-moi danser.mp3",
   "4-08 Allumer le feu.mp3"
 ];
-
-window.PLAYLIST_SQ = [
+window.PLAYLIST_SQ=[
   "2m37s_1-34 Les Champs-Elysées_x4.mp3",
   "2m43s_1-18 Je l'aime à mourir_x4.mp3",
   "2m59s_1-35 La Vie En Rose_x4.mp3",
@@ -69,140 +67,4 @@ window.PLAYLIST_SQ = [
   "Place des Grands Hommes.mp3",
   "Tu m'oublieras.mp3",
   "À nos souvenirs.mp3"
-];
-
-// Associations manuelles FR ↔ AL (éditez via config.html)
-window.PLAYLIST_PAIRS = [
-  {
-    "fr": "01-001 Gabriel.mp3",
-    "sq": "Gabriel.mp3"
-  },
-  {
-    "fr": "1-01 L'aventurier.mp3",
-    "sq": "L'aventurier.mp3"
-  },
-  {
-    "fr": "1-02 Tu m'oublieras.mp3",
-    "sq": "Tu m'oublieras.mp3"
-  },
-  {
-    "fr": "1-03 Ces soirées-là.mp3",
-    "sq": "3m18s_1-03 Ces soirées-là_x4.mp3"
-  },
-  {
-    "fr": "1-04 Savoir aimer.mp3",
-    "sq": "4m41s_1-04 Savoir aimer_x4.mp3"
-  },
-  {
-    "fr": "1-05 Tchikita.mp3",
-    "sq": "3m14s_1-05 Tchikita_x4.mp3"
-  },
-  {
-    "fr": "1-06 Emmenez-moi.mp3",
-    "sq": "3m31s_1-06 Emmenez-moi_x4.mp3"
-  },
-  {
-    "fr": "1-07 À nos souvenirs.mp3",
-    "sq": "À nos souvenirs.mp3"
-  },
-  {
-    "fr": "1-09 Djadja.mp3",
-    "sq": "DjaDja.mp3"
-  },
-  {
-    "fr": "1-11 Foule sentimentale.mp3",
-    "sq": "5m24s_1-11 Foule sentimentale_x4.mp3"
-  },
-  {
-    "fr": "1-12 La tribu de Dana.mp3",
-    "sq": "La tribu de dana.mp3"
-  },
-  {
-    "fr": "1-13 Wati by Night.mp3",
-    "sq": "4m8s_1-13 Wati by Night_x4.mp3"
-  },
-  {
-    "fr": "1-14 Ma philosophie.mp3",
-    "sq": "3m23s_1-14 Ma philosophie_x4.mp3"
-  },
-  {
-    "fr": "1-16 Elle a les yeux revolver.mp3",
-    "sq": "3m36s_1-16 Elle a les yeux revolver_x4.mp3"
-  },
-  {
-    "fr": "1-17 Alors on danse - Radio Edit.mp3",
-    "sq": "3m26s_1-17 Alors on danse - Radio Edit_x4.mp3"
-  },
-  {
-    "fr": "1-18 Je l'aime à mourir.mp3",
-    "sq": "2m43s_1-18 Je l'aime à mourir_x4.mp3"
-  },
-  {
-    "fr": "1-19 Les sunlights des tropiques.mp3",
-    "sq": null
-  },
-  {
-    "fr": "1-20 Balance ton quoi.mp3",
-    "sq": "3m9s_1-20 Balance ton quoi_x4.mp3"
-  },
-  {
-    "fr": "1-21 J'aimerais trop (feat. SAP).mp3",
-    "sq": "3m46s_1-21 J'aimerais trop (feat. SAP)_x4.mp3"
-  },
-  {
-    "fr": "1-22 Le lundi au soleil.mp3",
-    "sq": "Le lundi au soleil.mp3"
-  },
-  {
-    "fr": "1-23 Dernière danse.mp3",
-    "sq": "3m33s_1-23 Dernière danse_x4.mp3"
-  },
-  {
-    "fr": "1-24 Place des grands hommes.mp3",
-    "sq": "Place des Grands Hommes.mp3"
-  },
-  {
-    "fr": "1-25 Angela.mp3",
-    "sq": "Angela.mp3"
-  },
-  {
-    "fr": "1-28 Nuit de folie.mp3",
-    "sq": "4m13s_1-28 Nuit de folie_x4.mp3"
-  },
-  {
-    "fr": "1-29 Les lacs du Connemara.mp3",
-    "sq": "6m3s_1-29 Les lacs du Connemara_x4.mp3"
-  },
-  {
-    "fr": "1-30 J'irai où tu iras (with Jean-Jacques Goldman).mp3",
-    "sq": "J'irai ou tu iras.mp3"
-  },
-  {
-    "fr": "1-31 Les démons de minuit.mp3",
-    "sq": "3m51s_1-31 Les démons de minuit_x4.mp3"
-  },
-  {
-    "fr": "1-32 Mistral gagnant.mp3",
-    "sq": "Mistral Gagnant.mp3"
-  },
-  {
-    "fr": "1-33 Confessions Nocturnes.mp3",
-    "sq": "6m0s_1-33 Confessions Nocturnes_x4.mp3"
-  },
-  {
-    "fr": "1-34 Les Champs-Elysées.mp3",
-    "sq": "2m37s_1-34 Les Champs-Elysées_x4.mp3"
-  },
-  {
-    "fr": "1-35 La Vie En Rose.mp3",
-    "sq": "2m59s_1-35 La Vie En Rose_x4.mp3"
-  },
-  {
-    "fr": "2-15 Monday, Tuesday... Laissez-moi danser.mp3",
-    "sq": "Laissez-moi danser.mp3"
-  },
-  {
-    "fr": "4-08 Allumer le feu.mp3",
-    "sq": "5m29s_4-08 Allumer le feu_x4.mp3"
-  }
 ];

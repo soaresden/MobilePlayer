26f8906af4c5a7f1833d2fbe68d75c20f05b27b4
Denis SOARES <opx7a862u@mozmail.com>
2026-06-18 09:10:19 +0200

    fix

--- playlist.js
+++ playlist.js
@@ -1,4 +1,5 @@
-window.PLAYLIST_FR=[
+// Généré par config.html — 18/06/2026 09:09:58
+window.PLAYLIST_FR = [
   "01-001 Gabriel.mp3",
   "1-01 L'aventurier.mp3",
   "1-02 Tu m'oublieras.mp3",
@@ -33,30 +34,18 @@ window.PLAYLIST_FR=[
   "2-15 Monday, Tuesday... Laissez-moi danser.mp3",
   "4-08 Allumer le feu.mp3"
 ];
-window.PLAYLIST_SQ=[
+
+window.PLAYLIST_SQ = [
   "2m37s_1-34 Les Champs-Elysées_x4.mp3",
   "2m43s_1-18 Je l'aime à mourir_x4.mp3",
   "2m59s_1-35 La Vie En Rose_x4.mp3",
-  "3m14s_1-05 Tchikita_x4.mp3",
-  "3m18s_1-03 Ces soirées-là_x4.mp3",
-  "3m23s_1-14 Ma philosophie_x4.mp3",
   "3m26s_1-17 Alors on danse - Radio Edit_x4.mp3",
-  "3m31s_1-06 Emmenez-moi_x4.mp3",
   "3m33s_1-23 Dernière danse_x4.mp3",
   "3m36s_1-16 Elle a les yeux revolver_x4.mp3",
-  "3m46s_1-21 J'aimerais trop (feat. SAP)_x4.mp3",
-  "3m46s_1-25 Angela_x4.mp3",
-  "3m51s_1-31 Les démons de minuit_x4.mp3",
   "3m9s_1-20 Balance ton quoi_x4.mp3",
-  "4m13s_1-28 Nuit de folie_x4.mp3",
-  "4m41s_1-04 Savoir aimer_x4.mp3",
-  "4m8s_1-13 Wati by Night_x4.mp3",
-  "5m24s_1-11 Foule sentimentale_x4.mp3",
-  "5m29s_4-08 Allumer le feu_x4.mp3",
-  "6m0s_1-33 Confessions Nocturnes_x4.mp3",
-  "6m3s_1-29 Les lacs du Connemara_x4.mp3",
   "Angela - Saian Supa Crew.mp3",
   "DjaDja - Aya Nakamura.mp3",
+  "Emmenez-moi.mp3",
   "Gabriel - Najoua Belyzel.mp3",
   "J'Irai ou Tu Iras - Celine Dion.mp3",
   "L'Aventurier - Indochine.mp3",
@@ -65,6 +54,143 @@ window.PLAYLIST_SQ=[
   "Le Lundi Au Soleil - Claude Francois.mp3",
   "Mistral Gagnant - Renaud.mp3",
   "Place des Grands Hommes - Patrick Bruel.mp3",
+  "Savoir Aimer.mp3",
   "Tu M'Oublieras - Larusso.mp3",
   "À nos Souvenirs - Trois Cafe Gourmands.mp3"
+];
+
+// Associations manuelles FR ↔ AL (éditez via config.html)
+window.PLAYLIST_PAIRS = [
+  {
+    "fr": "01-001 Gabriel.mp3",
+    "sq": "Gabriel - Najoua Belyzel.mp3"
+  },
+  {
+    "fr": "1-01 L'aventurier.mp3",
+    "sq": "L'Aventurier - Indochine.mp3"
+  },
+  {
+    "fr": "1-02 Tu m'oublieras.mp3",
+    "sq": "Tu M'Oublieras - Larusso.mp3"
+  },
+  {
+    "fr": "1-03 Ces soirées-là.mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-04 Savoir aimer.mp3",
+    "sq": "Savoir Aimer.mp3"
+  },
+  {
+    "fr": "1-05 Tchikita.mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-06 Emmenez-moi.mp3",
+    "sq": "Emmenez-moi.mp3"
+  },
+  {
+    "fr": "1-07 À nos souvenirs.mp3",
+    "sq": "À nos Souvenirs - Trois Cafe Gourmands.mp3"
+  },
+  {
+    "fr": "1-09 Djadja.mp3",
+    "sq": "DjaDja - Aya Nakamura.mp3"
+  },
+  {
+    "fr": "1-11 Foule sentimentale.mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-12 La tribu de Dana.mp3",
+    "sq": "La Tribu de Dana - Manau.mp3"
+  },
+  {
+    "fr": "1-13 Wati by Night.mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-14 Ma philosophie.mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-16 Elle a les yeux revolver.mp3",
+    "sq": "3m36s_1-16 Elle a les yeux revolver_x4.mp3"
+  },
+  {
+    "fr": "1-17 Alors on danse - Radio Edit.mp3",
+    "sq": "3m26s_1-17 Alors on danse - Radio Edit_x4.mp3"
+  },
+  {
+    "fr": "1-18 Je l'aime à mourir.mp3",
+    "sq": "2m43s_1-18 Je l'aime à mourir_x4.mp3"
+  },
+  {
+    "fr": "1-19 Les sunlights des tropiques.mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-20 Balance ton quoi.mp3",
+    "sq": "3m9s_1-20 Balance ton quoi_x4.mp3"
+  },
+  {
+    "fr": "1-21 J'aimerais trop (feat. SAP).mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-22 Le lundi au soleil.mp3",
+    "sq": "Le Lundi Au Soleil - Claude Francois.mp3"
+  },
+  {
+    "fr": "1-23 Dernière danse.mp3",
+    "sq": "3m33s_1-23 Dernière danse_x4.mp3"
+  },
+  {
+    "fr": "1-24 Place des grands hommes.mp3",
+    "sq": "Place des Grands Hommes - Patrick Bruel.mp3"
+  },
+  {
+    "fr": "1-25 Angela.mp3",
+    "sq": "Angela - Saian Supa Crew.mp3"
+  },
+  {
+    "fr": "1-28 Nuit de folie.mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-29 Les lacs du Connemara.mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-30 J'irai où tu iras (with Jean-Jacques Goldman).mp3",
+    "sq": "J'Irai ou Tu Iras - Celine Dion.mp3"
+  },
+  {
+    "fr": "1-31 Les démons de minuit.mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-32 Mistral gagnant.mp3",
+    "sq": "Mistral Gagnant - Renaud.mp3"
+  },
+  {
+    "fr": "1-33 Confessions Nocturnes.mp3",
+    "sq": null
+  },
+  {
+    "fr": "1-34 Les Champs-Elysées.mp3",
+    "sq": "2m37s_1-34 Les Champs-Elysées_x4.mp3"
+  },
+  {
+    "fr": "1-35 La Vie En Rose.mp3",
+    "sq": "2m59s_1-35 La Vie En Rose_x4.mp3"
+  },
+  {
+    "fr": "2-15 Monday, Tuesday... Laissez-moi danser.mp3",
+    "sq": null
+  },
+  {
+    "fr": "4-08 Allumer le feu.mp3",
+    "sq": null
+  }
 ];
